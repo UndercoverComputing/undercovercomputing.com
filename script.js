1569c0dada968d60c1e8e82a415cdcb96f5c5a39
Undercover Computing <undercover.computing@gmail.com>
2024-10-29 10:24:50 +1300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function copyEmail(event) {
     event.preventDefault();
 
     // Copy the email to clipboard
-    navigator.clipboard.writeText("undercovercomputing@gmail.com").then(function() {
+    navigator.clipboard.writeText("undercover.computing@gmail.com").then(function() {
         // Display the confirmation message
         document.getElementById("confirmation-message").style.display = "block";
         
